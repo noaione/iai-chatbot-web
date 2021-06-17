2bd6286c00cf056380b4dfccc3b33e370e928edd
noaione <noaione0809@gmail.com>
2021-06-17 11:47:14 +0700

    pages: add title and description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Head from "next/head";
+
 import { useScript } from "../lib/script";
 
 interface GroupMemberProps {
@@ -31,52 +33,63 @@ export default function MainHomepage() {
         )}`
     );
     return (
-        <div className="flex flex-col">
-            <div className="flex flex-col justify-center items-center p-5">
-                <h1 className="font-bold text-4xl mt-8">A Food Chatbot</h1>
-                <h3 className="text-xl mt-2 font-semibold">Created by: Group 15</h3>
-                {status !== "ready" ? (
-                    <p>Please wait until our Bot is ready...</p>
-                ) : (
-                    <p className="whitespace-pre-wrap mt-1">
-                        Our bot should be loaded now, please check your bottom-right screen for the popup
-                        button
+        <>
+            <Head>
+                <title>IAI Chatbot - Group 15</title>
+                <meta name="description" content="A Food Chatbot made by Group 15 for IAI Project" />
+            </Head>
+            <div className="flex flex-col">
+                <div className="flex flex-col justify-center items-center p-5">
+                    <h1 className="font-bold text-4xl mt-8">A Food Chatbot</h1>
+                    <h3 className="text-xl mt-2 font-semibold">Created by: Group 15</h3>
+                    {status !== "ready" ? (
+                        <p>Please wait until our Bot is ready...</p>
+                    ) : (
+                        <p className="whitespace-pre-wrap mt-1">
+                            Our bot should be loaded now, please check your bottom-right screen for the popup
+                            button
+                        </p>
+                    )}
+                </div>
+                <hr className="mt-5 mb-2" />
+                <div className="flex flex-col p-5">
+                    <h2 className="text-2xl mb-2 font-semibold">About</h2>
+                    <p>
+                        This bot was created for our Class Project of Introduction to Artificial Intelligence.
                     </p>
-                )}
-            </div>
-            <hr className="mt-5 mb-2" />
-            <div className="flex flex-col p-5">
-                <h2 className="text-2xl mb-2 font-semibold">About</h2>
-                <p>This bot was created for our Class Project of Introduction to Artificial Intelligence.</p>
-                <p>Our group picked a Problem Domain of Food related stuff.</p>
-                <p>Our group consist of:</p>
-                <ol className="list-decimal list-inside gap-2">
-                    <GroupMember name="Gabrielly Caroline" tp="" />
-                    <GroupMember name="Kevin Prasetia Kumala" tp="058198" />
-                    <GroupMember name="Muhammad I'zaz Al-Aiman Maharana Hart" tp="062341" />
-                    <GroupMember name="Valeria Angela Liviany" tp="058782" />
-                </ol>
-                <p className="mt-1">
-                    If you have a second, we would like you to fill our questionaire about our Bot:
-                </p>
-                <div className="mt-1 ml-1">
+                    <p>Our group picked a Problem Domain of Food related stuff.</p>
+                    <p>Our group consist of:</p>
+                    <ol className="list-decimal list-inside gap-2">
+                        <GroupMember name="Gabrielly Caroline" tp="" />
+                        <GroupMember name="Kevin Prasetia Kumala" tp="058198" />
+                        <GroupMember name="Muhammad I'zaz Al-Aiman Maharana Hart" tp="062341" />
+                        <GroupMember name="Valeria Angela Liviany" tp="058782" />
+                    </ol>
+                    <p className="mt-1">
+                        If you have a second, we would like you to fill our questionaire about our Bot:
+                    </p>
+                    <div className="mt-1 ml-1">
+                        <a
+                            className="hover:underline text-red-400"
+                            href="https://forms.gle/o4DRgkfuBPWht8Vb6"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            &lt;&lt; Click here &gt;&gt;
+                        </a>
+                    </div>
+                </div>
+                <hr className="mt-2 mb-2" />
+                <div className="flex flex-row gap-1 p-5 justify-center">
+                    <span>This website is Open Source </span>
                     <a
-                        className="hover:underline text-red-400"
-                        href="https://forms.gle/o4DRgkfuBPWht8Vb6"
-                        target="_blank"
-                        rel="noreferrer noopener"
+                        className="text-red-400 hover:underline"
+                        href="https://github.com/noaione/iai-chatbot-web"
                     >
-                        &lt;&lt; Click here &gt;&gt;
+                        See on GitHub
                     </a>
                 </div>
             </div>
-            <hr className="mt-2 mb-2" />
-            <div className="flex flex-row gap-1 p-5 justify-center">
-                <span>This website is Open Source </span>
-                <a className="text-red-400 hover:underline" href="https://github.com/noaione/iai-chatbot-web">
-                    See on GitHub
-                </a>
-            </div>
-        </div>
+        </>
     );
 }
