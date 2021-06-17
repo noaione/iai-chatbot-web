877c0d53dabdfbe2e3127fcfc8f6a933b21e729b
noaione <noaione0809@gmail.com>
2021-06-17 11:39:51 +0700

    pages: remove unnecessary data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,6 @@
 import React from "react";
 import { useScript } from "../lib/script";
 
-const DATA = `
-<script>
-!(function (e, t, a) {
-    var c = e.head || e.getElementsByTagName("head")[0],
-        n = e.createElement("script");
-    (n.async = !0), (n.defer = !0), (n.type = "text/javascript"), (n.src = t + "/static/js/chat_widget.js?config=" + JSON.stringify(a)), c.appendChild(n);
-})(document, "https://app.engati.com", { bot_key: "5ad563cf58684af5", welcome_msg: true, branding_key: "default", server: "https://app.engati.com", e: "p" });
-
-</script>`;
-
 interface GroupMemberProps {
     name: string;
     tp: string;
