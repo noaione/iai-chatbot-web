61b59085c6be214eba0169da929cc79374842244
noaione <noaione0809@gmail.com>
2021-06-26 13:21:05 +0700

    typing: add global type

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,6 +3,9 @@
 
 declare global {
     namespace NodeJS {
+        interface Global {
+            cachedRecipes: any[];
+        }
         interface ProcessEnv {
             EDAMAM_APP_ID?: string;
             EDAMAM_APP_SECRET?: string;
