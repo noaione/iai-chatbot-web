addb9d55da86787a83afd6c89242583c50b9c2ee
noaione <noaione0809@gmail.com>
2021-06-26 11:14:44 +0700

    typing: add global process.env typing

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,11 @@
-
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            EDAMAM_APP_ID?: string;
+            EDAMAM_APP_SECRET?: string;
+        }
+    }
+}
