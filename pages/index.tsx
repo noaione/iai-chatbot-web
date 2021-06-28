04425aac21932bfc65354244ed1341a219b32d12
noaione <noaione0809@gmail.com>
2021-06-28 17:14:33 +0700

    pages: some adjustment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Head from "next/head";
+import Image from "next/image";
 
 import { useScript } from "../lib/script";
+
+interface CommentData {
+    children?: string;
+}
+
+function createComment(comment?: string) {
+    if (!comment) {
+        return `<!-- (empty) -->`;
+    }
+    if (comment.includes("\n")) {
+        return `<!--\n${comment}\n-->`;
+    }
+    return `<!-- ${comment} -->`;
+}
+
+function ReactComment(props: CommentData) {
+    const el = useRef<HTMLDivElement>();
+    useEffect(() => {
+        if (el?.current) {
+            try {
+                el.current.outerHTML = createComment(props.children);
+            } catch (e) {
+                console.error("[ReactComment] Failed to mount the comment data to an Element!");
+            }
+        }
+    }, []);
+
+    return <div ref={el} />;
+}
 
 interface GroupMemberProps {
     name: string;
@@ -38,9 +68,18 @@ export default function MainHomepage() {
                 <title>IAI Chatbot - Group 15</title>
                 <meta name="description" content="A Food Chatbot made by Group 15 for IAI Project" />
             </Head>
+            <ReactComment>Website created with Next.JS and React</ReactComment>
+            <ReactComment>The website also have public API that you can possibly use</ReactComment>
+            <ReactComment>
+                Just check the source code for more info:
+                https://github.com/noaione/iai-chatbot-web/tree/master/pages/api
+            </ReactComment>
             <div className="flex flex-col">
                 <div className="flex flex-col justify-center items-center p-5">
-                    <h1 className="font-bold text-4xl mt-8">A Food Chatbot</h1>
+                    <div className="flex flex-col justify-center mt-8">
+                        <Image src="/apple-icon-114x114.png" width={114} height={114} alt="The Cake Icon" />
+                    </div>
+                    <h1 className="font-bold text-4xl mt-4">A Food Chatbot</h1>
                     <h3 className="text-xl mt-2 font-semibold">Created by: Group 15</h3>
                     {status !== "ready" ? (
                         <p>Please wait until our Bot is ready...</p>
@@ -60,7 +99,7 @@ export default function MainHomepage() {
                     <p>Our group picked a Problem Domain of Food related stuff.</p>
                     <p>Our group consist of:</p>
                     <ol className="list-decimal list-inside gap-2">
-                        <GroupMember name="Gabrielly Caroline" tp="" />
+                        <GroupMember name="Gabrielly Caroline" tp="061805" />
                         <GroupMember name="Kevin Prasetia Kumala" tp="058198" />
                         <GroupMember name="Muhammad I'zaz Al-Aiman Maharana Hart" tp="062341" />
                         <GroupMember name="Valeria Angela Liviany" tp="058782" />
@@ -85,6 +124,8 @@ export default function MainHomepage() {
                     <a
                         className="text-red-400 hover:underline"
                         href="https://github.com/noaione/iai-chatbot-web"
+                        target="_blank"
+                        rel="noreferrer noopener"
                     >
                         See on GitHub
                     </a>
